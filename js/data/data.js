1c7d333186d6d9e23447faa7e95f32efe8829f64
its-gio <me@its-g.io>
2018-09-16 16:52:43 -0700

    {Adding data for post, groupd, and content} and adding the group list to the aside.

---
+++ js/data/data.js
@@ -0,0 +1,53 @@
+const data = [{
+    "group1": [
+        {
+            "post1": [
+                {
+                    "Title": "This is a test",
+                    "Sub-Title": "Testing the subtitle",
+                    "Content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fringilla nulla quis nisi porta, id mattis risus rutrum. Nunc consectetur lectus a nibh ornare, eget ultrices sem vehicula. Vivamus viverra congue sem, et egestas lacus vestibulum vitae. Pellentesque magna mauris, convallis ac accumsan et, luctus non quam. Sed ac leo nibh. Proin elementum posuere tellus, ut facilisis tortor. Vivamus bibendum nunc ac dui pretium, vitae elementum nibh hendrerit. Phasellus congue euismod ante ullamcorper ornare. Maecenas gravida dapibus metus, a aliquam odio pretium id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse tempus, nulla ac mollis congue, tortor arcu scelerisque turpis, eu vulputate urna ligula euismod urna. Nam finibus blandit pretium. Pellentesque ultricies dolor ac suscipit vestibulum. Suspendisse sodales tincidunt accumsan. Integer a luctus nunc."
+                }
+            ]
+        }
+    ]
+},
+{
+    "group2": [
+        {
+            "post1": [
+                {
+                    "Title": "This is a test",
+                    "Sub-Title": "Testing the subtitle",
+                    "Content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fringilla nulla quis nisi porta, id mattis risus rutrum. Nunc consectetur lectus a nibh ornare, eget ultrices sem vehicula. Vivamus viverra congue sem, et egestas lacus vestibulum vitae. Pellentesque magna mauris, convallis ac accumsan et, luctus non quam. Sed ac leo nibh. Proin elementum posuere tellus, ut facilisis tortor. Vivamus bibendum nunc ac dui pretium, vitae elementum nibh hendrerit. Phasellus congue euismod ante ullamcorper ornare. Maecenas gravida dapibus metus, a aliquam odio pretium id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse tempus, nulla ac mollis congue, tortor arcu scelerisque turpis, eu vulputate urna ligula euismod urna. Nam finibus blandit pretium. Pellentesque ultricies dolor ac suscipit vestibulum. Suspendisse sodales tincidunt accumsan. Integer a luctus nunc."
+                }
+            ]
+        }
+    ]
+},
+{
+    "group3": [
+        {
+            "post1": [
+                {
+                    "Title": "This is a test",
+                    "Sub-Title": "Testing the subtitle",
+                    "Content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fringilla nulla quis nisi porta, id mattis risus rutrum. Nunc consectetur lectus a nibh ornare, eget ultrices sem vehicula. Vivamus viverra congue sem, et egestas lacus vestibulum vitae. Pellentesque magna mauris, convallis ac accumsan et, luctus non quam. Sed ac leo nibh. Proin elementum posuere tellus, ut facilisis tortor. Vivamus bibendum nunc ac dui pretium, vitae elementum nibh hendrerit. Phasellus congue euismod ante ullamcorper ornare. Maecenas gravida dapibus metus, a aliquam odio pretium id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse tempus, nulla ac mollis congue, tortor arcu scelerisque turpis, eu vulputate urna ligula euismod urna. Nam finibus blandit pretium. Pellentesque ultricies dolor ac suscipit vestibulum. Suspendisse sodales tincidunt accumsan. Integer a luctus nunc"
+                }
+            ]
+        }
+    ]
+},
+{
+    "group4": [
+        {
+            "post1": [
+                {
+                    "Title": "This is a test",
+                    "Sub-Title": "Testing the subtitle",
+                    "Content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fringilla nulla quis nisi porta, id mattis risus rutrum. Nunc consectetur lectus a nibh ornare, eget ultrices sem vehicula. Vivamus viverra congue sem, et egestas lacus vestibulum vitae. Pellentesque magna mauris, convallis ac accumsan et, luctus non quam. Sed ac leo nibh. Proin elementum posuere tellus, ut facilisis tortor. Vivamus bibendum nunc ac dui pretium, vitae elementum nibh hendrerit. Phasellus congue euismod ante ullamcorper ornare. Maecenas gravida dapibus metus, a aliquam odio pretium id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse tempus, nulla ac mollis congue, tortor arcu scelerisque turpis, eu vulputate urna ligula euismod urna. Nam finibus blandit pretium. Pellentesque ultricies dolor ac suscipit vestibulum. Suspendisse sodales tincidunt accumsan. Integer a luctus nunc."
+                }
+            ]
+        }
+    ]
+}
+];
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-console.log("Working!")
+const group = document.querySelector(".groups");
+
+data.forEach(() => {
+    group.appendChild(document.createElement("li"));
+})
