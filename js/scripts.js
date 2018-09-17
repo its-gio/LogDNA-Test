a3d28ddef98f31397e02bef4e794672b75699763
its-gio <me@its-g.io>
2018-09-16 18:00:04 -0700

    Added sidebar groups and image to look like image depending on how many groups there are

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 const group = document.querySelector(".groups");
 
-data.forEach(() => {
-    group.appendChild(document.createElement("li"));
-})
+// Adds a list item to unordered list with an image and h5 text
+data.forEach((data, i) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const groupNum = document.createElement("h5");
+    const index = document.createTextNode(`⌘ ${i + 1}`);
+    li.appendChild(img).classList.add("group--image");
+    li.appendChild(groupNum).appendChild(index);
+    li.appendChild(groupNum).classList.add("group--index");
+    group.appendChild(li).classList.add("group", `group${i + 1}`);
+});
