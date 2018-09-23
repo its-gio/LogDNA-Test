e6f3c5c723742ca70f290904a09907cf67b822da
its-gio <me@its-g.io>
2018-09-22 21:43:41 -0700

    Added an edge case to the posts list and made an event listener for single post content.

--- js/scripts.js
+++ js/scripts.js
@@ -11,21 +11,29 @@ function handleActiveGroup(liCoords) {
     groupMarker.style.setProperty('transform', `translate(0px, ${calcCoords.top}px)`);
 }
 
-function getContent(index) {
-    console.log(index);
+function getContent(content, i) {
+    console.log(content[`post${i}`]);
 };
 
 
 function grabPosts(data, i) {
     // data[`group${i + 1}`].forEach(text => console.log(text));
-    const template = data[`group${i + 1}`].map((content, index) => {
-        let Text = content[`post${index + 1}`];
-        return `<div class="posts--list-item">
+    if (posts.innerHTML != "") posts.innerHTML = "";
+    data[`group${i}`].forEach((content, count) => {
+        let Text = content[`post${count + 1}`];
+        const postListItem = document.createElement("div");
+        postListItem.classList.add("posts--list-item");
+        const preview = `
             <h5 class="posts--list-item__heading">${Text.Title}</h5>
             <p class="posts--list-item__content">${Text.Content.substring(0, 230)}...</p>
-        </div>`;
-    }).join('');
-    posts.innerHTML = template;
+        `;
+        postListItem.innerHTML = preview;
+        postListItem.addEventListener("click", function () {
+            getContent(content, count + 1);
+        });
+        posts.appendChild(postListItem);
+    });
+    console.log();
 }
 
 // Adds a list item to unordered list with an image and h5 text
@@ -34,7 +42,7 @@ function createGroups(data, i) {
     const li = document.createElement("li");
     const img = document.createElement("img");
     const groupNum = document.createElement("h5");
-    const index = document.createTextNode(`⌘ ${i + 1}`);
+    const index = document.createTextNode(`⌘ ${i}`);
     // Appends img with a class of group--image to list item
     li.appendChild(img).classList.add("group--image");
     // Appending h5 to list item element with the text
@@ -45,12 +53,12 @@ function createGroups(data, i) {
     li.addEventListener("click", function () {
         // Populates posts list
         grabPosts(data, i);
-        // Moves active group when clicked
+        // Moves active group marker when clicked
         handleActiveGroup(li.getBoundingClientRect());
     });
     // Appending List item element to unorded list element with classnames
-    groups.appendChild(li).classList.add("group", `group${i + 1}`);
+    groups.appendChild(li).classList.add("group", `group${i}`);
 }
 
 // Using data for function above
-data.forEach((data, i) => createGroups(data, i));
+data.forEach((data, i) => createGroups(data, i + 1));
